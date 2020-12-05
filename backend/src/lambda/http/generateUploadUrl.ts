33e1a2eeaf801feb72fee5b0ee6cfbdde60416a6
JoelColucci <joelcolucci@icloud.com>
2020-12-04 22:11:03 -0500

    implement generateUploadUrl and attachment resources

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,30 @@
 import 'source-map-support/register'
 
+import * as AWS from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
+const attachmentsBucket = process.env.ATTACHMENTS_BUCKET
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const s3 = new AWS.S3({
+    signatureVersion: 'v4'
+  })
+  const uploadUrl = s3.getSignedUrl('putObject', {
+    Bucket: attachmentsBucket, 
+    Key: todoId,
+    Expires: 300
+  })
+  
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      uploadUrl
+    })
+  }
 }
